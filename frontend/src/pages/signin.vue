7d66279ebdd2cf8ed322101026491c9188e2b90e
atsushi0124 <h19170004@gmail.com>
2023-12-20 12:47:21 +0900

    サインイン画面の作成

--- frontend/src/pages/signin.vue
+++ frontend/src/pages/signin.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { useField, useForm } from 'vee-validate'
-import { useUserStore } from '~/store/user'
-import { SignInResponse } from '~/types/api'
+import { useField, useForm } from "vee-validate";
+import { useUserStore } from "~/store/user";
+import { SignInResponse } from "~/types/api";
 
 // 環境変数（.env参照）からAPIのベースURLを取得
-const $config = useRuntimeConfig()
-const apiBaseUrl = $config.public.apiBaseUrl
+const $config = useRuntimeConfig();
+const apiBaseUrl = $config.public.apiBaseUrl;
 // ユーザーストアを取得
-const userStore = useUserStore()
+const userStore = useUserStore();
 // 転送処理を行うためのフック
-const $router = useRouter()
+const $router = useRouter();
 
 // フォームの設定
 const { handleSubmit, isSubmitting } = useForm({
@@ -18,28 +18,28 @@ const { handleSubmit, isSubmitting } = useForm({
     email(value: string) {
       // 必須
       if (!value) {
-        return 'メールアドレスを入力してください'
+        return "メールアドレスを入力してください";
       }
       // メールアドレスの形式
       const emailPattern =
-        /^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/
+        /^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
       if (!emailPattern.test(value)) {
-        return 'メールアドレスの形式が正しくありません'
+        return "メールアドレスの形式が正しくありません";
       }
-      return true
+      return true;
     },
     password(value: string) {
       if (!value) {
-        return 'パスワードを入力してください'
+        return "パスワードを入力してください";
       }
-      return true
+      return true;
     },
   },
-})
+});
 // フィールドの値とエラーメッセージを取得
-const { value: email, errorMessage: emailErrorMessage } = useField('email')
+const { value: email, errorMessage: emailErrorMessage } = useField("email");
 const { value: password, errorMessage: passwordErrorMessage } =
-  useField('password')
+  useField("password");
 
 /**
  * サインイン処理
@@ -50,67 +50,153 @@ const onSubmit = handleSubmit(async () => {
     const { data } = await useFetch<SignInResponse>(
       `${apiBaseUrl}/auth/signin`,
       {
-        method: 'POST',
+        method: "POST",
         body: {
           email: email.value,
           password: password.value,
         },
       }
-    )
+    );
 
     // レスポンスのデータを取得（ref値）
-    const response = data.value
+    const response = data.value;
     // トークンの有無でログインできたか判断
-    const hasToken = response && !!response.token
+    const hasToken = response && !!response.token;
     if (hasToken) {
       // 成功の場合はトークンを保存
-      userStore.token = response.token
-      userStore.email = response.email
+      userStore.token = response.token;
+      userStore.email = response.email;
       // トップページに遷移
-      $router.push('/')
+      $router.push("/");
     } else {
       // 失敗の場合はフィールドをクリア
-      email.value = ''
-      password.value = ''
+      email.value = "";
+      password.value = "";
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
 // ページのタイトルなどを設定
 useHead({
-  title: 'サインインページ',
-})
+  title: "サインインページ",
+});
 </script>
 
 <template>
-  <div>
-    <h2>サインイン</h2>
-    <form @submit="onSubmit">
-      <input v-model="email" name="email" type="email" />
-      <p class="error">{{ emailErrorMessage }}</p>
-      <input
-        v-model="password"
-        name="password"
-        type="password"
-        autocomplete="on"
-      />
-      <p class="error">{{ passwordErrorMessage }}</p>
-      <button type="submit" :disabled="isSubmitting">送信</button>
-    </form>
+  <div class="Container">
+    <div class="Block__main">
+      <h2>サインイン</h2>
+      <form @submit="onSubmit">
+        <label for="E-mail">
+          <p class="label">E-mail</p>
+          <p class="error">{{ emailErrorMessage }}</p>
+          <input
+            v-model="email"
+            class="email__input"
+            name="email"
+            type="email"
+            id="E-mail"
+          />
+        </label>
+
+        <label for="PassWord">
+          <p class="label">パスワード</p>
+          <p class="error">{{ passwordErrorMessage }}</p>
+          <input
+            v-model="password"
+            name="password"
+            type="password"
+            autocomplete="on"
+            id="PassWord"
+            class="passWord__input"
+          />
+        </label>
+        <button class="submit__btn" type="submit" :disabled="isSubmitting">
+          Sign In
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+// ブロックのcss
+.Container {
+  position: relative;
+  .Block__main {
+    height: auto;
+    width: 50%;
+    max-width: 600px;
+    background: #cdcdcd;
+    text-align: center;
+    display: flex;
+    justify-content: space-evenly;
+    margin: 0 auto;
+    flex-direction: column;
+    border-radius: 10px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, 35%);
+  }
+}
+
+// フォームのcss
+form {
+  width: 70%;
+  margin: 0 auto;
+  label {
+    display: block;
+    .label {
+      text-align: left;
+      margin: 5px 0;
+    }
+    input {
+      width: 100%;
+      height: 35px;
+      border-radius: 5px;
+      border: none;
+    }
+    .email__input {
+      margin-bottom: 20px;
+    }
+    .passWord__input {
+      margin-bottom: 60px;
+    }
+  }
+
+  // ボタンのcss
+  .submit__btn {
+    width: 100%;
+    margin-bottom: 50px;
+    height: 35px;
+    font-weight: 400;
+    border-radius: 5px;
+    border: none;
+    background-color: $colorPurple;
+    color: $colorWhite;
+    border: $colorPurple solid 2px;
+    transition: 0.5s;
+    &:hover {
+      background-color: $colorWhite;
+      color: $colorPurple;
+    }
+  }
+}
+
 h2 {
+  padding: 30px;
   font-size: 1.8rem;
   @media #{$pc} {
     font-size: 2.4rem;
   }
 }
 .error {
+  text-align: left;
   /* ~/assets/style/_variables.scssで定義した変数を使うことができます */
   color: $colorRed;
+  margin-top: -5px;
 }
 </style>
